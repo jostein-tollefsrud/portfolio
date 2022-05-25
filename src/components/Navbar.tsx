9458c7bfedc7587104cb1da220e83e4b0745bd0c
jostein-tollefsrud <mail@josteintollefsrud.no>
2022-05-25 22:47:20 +0200

    add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,57 @@
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import Logo from './Logo';
 
 const Navbar: FC = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <header>
-      <nav aria-label='main navigation'>
-        <ul>
-          <li>
-            <Link href='/'>
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/projects'>
-              <a>Projects</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/about'>
-              <a>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/contact'>
-              <a>Contact</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <header className='header'>
+      <div className='header__container'>
+        <Link href={'/'}>
+          <a aria-label='home'>
+            <Logo className='header__logo' />
+          </a>
+        </Link>
+
+        <button
+          aria-label='menu'
+          className={`header__button ${open && 'active'}`}
+          onClick={() => setOpen(!open)}
+        >
+          <span className='header__icon-1'></span>
+          <span className='header__icon-2'></span>
+          <span className='header__icon-3'></span>
+        </button>
+
+        <nav
+          aria-label='main navigation'
+          className={`${open && 'open'}`}
+          onClick={() => setOpen(false)}
+        >
+          <ul>
+            <li>
+              <Link href='/'>
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='#projects'>
+                <a>Projects</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='#about'>
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='#contact'>
+                <a>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
