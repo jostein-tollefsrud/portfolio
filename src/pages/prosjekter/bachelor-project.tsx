714d594fefed4707549f2d0238bb1fb13f390ba0
jostein-tollefsrud <mail@josteintollefsrud.no>
2022-05-31 19:03:45 +0200

    finished writing

--- src/pages/prosjekter/bachelor-project.tsx
+++ src/pages/prosjekter/bachelor-project.tsx
@@ -65,6 +65,7 @@ const BachelorProjectPage: NextPage = () => {
             <ul className='techstack'>
               <li>Next.js</li>
               <li>React.js</li>
+              <li>Strapi</li>
               <li>Heroku</li>
               <li>Formik</li>
               <li>Yup</li>
@@ -78,33 +79,133 @@ const BachelorProjectPage: NextPage = () => {
             </ul>
           </div>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut
-            asperiores provident accusamus omnis cupiditate temporibus iusto!
-            Voluptatem, natus corporis! Eos vel error nemo soluta ipsa amet
-            placeat velit temporibus!
-          </p>
+          {locale === 'no' ? (
+            <>
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Bakgrunn
+              </h2>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut
-            asperiores provident accusamus omnis cupiditate temporibus iusto!
-            Voluptatem, natus corporis! Eos vel error nemo soluta ipsa amet
-            placeat velit temporibus!
-          </p>
+              <p className='u-margin-bottom-large'>
+                Etter tre år med studie ved NTNU Gjøvik skulle vi fullføre med
+                en{' '}
+                <a
+                  className='link'
+                  target='_blank'
+                  rel='noopner noreferrer'
+                  href='https://www.ntnu.no/studier/emner/IDG3920/2021#tab=omEmnet'
+                >
+                  bachelor oppgave
+                </a>
+                . Jeg og tre andre medstudenter ønsket å bygge videre på et
+                annet prosjekt vi tok i et annet fag hvor vi fikk tildelt en
+                ferdig prototype, men avdekket mangler ved denne noe som gjorde
+                at vi startet helt på nytt med design og kode i bachelor
+                oppgaven.
+              </p>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut
-            asperiores provident accusamus omnis cupiditate temporibus iusto!
-            Voluptatem, natus corporis! Eos vel error nemo soluta ipsa amet
-            placeat velit temporibus!
-          </p>
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Teknologier
+              </h2>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut
-            asperiores provident accusamus omnis cupiditate temporibus iusto!
-            Voluptatem, natus corporis! Eos vel error nemo soluta ipsa amet
-            placeat velit temporibus!
-          </p>
+              <p>
+                Denne nettsiden er laget med Next.js i frontend og Strapi for
+                backend. Prosjekteier ønsket et CMS hvor det ikke var nødvendig
+                å kunne kode eller HTML da de som skulle oppdatere innholdet
+                ikke hadde kompetanse på det feltet. Derfor falt valget på
+                Strapi. Det CMS systemet gjorde det også enkelt å få alt innhold
+                på norsk og engelsk pluss muligheten for å paginere innlegg de
+                skulle skrive.
+              </p>
+
+              <p className='u-margin-bottom-large'>
+                Vi brukte Sass for å style enkelte elementer, men i hovedsak
+                benyttet vi oss av biblioteket Material UI for komponenter.
+                Sentry ble brukt til å holde oversikt over feilmeldinger under
+                utviklingen og panelbear ble lagt til som et alternativ til
+                Google Analytics.
+              </p>
+
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Forbedringer
+              </h2>
+
+              <p>
+                Dersom jeg skulle gjort dette prosjektet på nytt ville jeg
+                testet ut andre CMS løsninger for å lære mer. I stedenfor å
+                bruke Material UI ville jeg stylet alt med Sass og kodet selv
+                for å ha mer kontroll på hvordan de oppfører seg.
+              </p>
+
+              <p>
+                Jeg ville fokusert mer på tilgjengelighet og testet det nøye for
+                hver komponent vi laget og implementert Jest og Testing library
+                for å teste hver komponent før de blir pusha ut til github.
+              </p>
+            </>
+          ) : (
+            <>
+              {/* ########## ENGLISH ########## */}
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Background
+              </h2>
+
+              <p className='u-margin-bottom-large'>
+                After three years of study at NTNU Gjøvik, we were to complete a{' '}
+                <a
+                  className='link'
+                  target='_blank'
+                  rel='noopner noreferrer'
+                  href='https://www.ntnu.edu/studies/courses/IDG3920/2021#tab=omEmnet'
+                >
+                  bachelor thesis
+                </a>
+                . I and three other fellow students wanted to build upon another
+                project we took in another subject where we were assigned a
+                finished prototype but revealed shortcomings in this which made
+                us start all over again with designing and coding in the
+                bachelor thesis.
+              </p>
+
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Technologies
+              </h2>
+
+              <p>
+                This website is made with Next.js for the frontend and Strapi
+                for the backend. The project owner wanted a CMS where it was not
+                necessary to be able to code or HTML as those who were to update
+                the content did not have expertise in that field. Therefore, the
+                choice fell on Strapi. The CMS system also made it easy to get
+                all content in Norwegian and English plus the ability to
+                paginate posts.
+              </p>
+
+              <p className='u-margin-bottom-large'>
+                We used Sass to style some elements, but we mainly used the
+                Material UI library for components. Sentry was used to keeping
+                track of error messages during development and Panelbear was
+                added as an alternative to Google Analytics.
+              </p>
+
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Improvements
+              </h2>
+
+              <p>
+                If I were to do this project again I would test out other CMS
+                solutions to learn more. Instead of using Material UI, I would
+                style everything with Sass and code myself to have more control
+                over how they behave.
+              </p>
+
+              <p>
+                I would focus more on accessibility and test it carefully for
+                each component we created and implement the Jest and Testing
+                library to test each component before being pushed out to
+                Github.
+              </p>
+            </>
+          )}
         </div>
       </main>
     </div>
--- src/pages/prosjekter/plantmanager.tsx
+++ src/pages/prosjekter/plantmanager.tsx
@@ -71,25 +71,112 @@ const PlantmanagerPage: NextPage = () => {
             </ul>
           </div>
 
-          <h2 className='heading-secondary u-margin-bottom-small'>Bakgrunn</h2>
+          {locale === 'no' ? (
+            <>
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Bakgrunn
+              </h2>
 
-          <p className='u-margin-bottom-large'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut
-            asperiores provident accusamus omnis cupiditate temporibus iusto!
-            Voluptatem, natus corporis! Eos vel error nemo soluta ipsa amet
-            placeat velit temporibus!
-          </p>
+              <p className='u-margin-bottom-large'>
+                På NTNUs lokale på Mustad har de mange planter å holde
+                oversikten over. Hvem har vannet, når ble de vannet sist og hvor
+                står de? Avdelingen for design trenger et administrativt verktøy
+                for å overvåke plantenes tilstand og helse. Dette prosjektet tar
+                seg av både backend API og frontend. Prosjektet er gruppearbeid
+                hvor vi var tre stykker som arbeidet på samme prosjekt.
+              </p>
 
-          <h2 className='heading-secondary u-margin-bottom-small'>
-            Forbedringer
-          </h2>
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Teknologier
+              </h2>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ut
-            asperiores provident accusamus omnis cupiditate temporibus iusto!
-            Voluptatem, natus corporis! Eos vel error nemo soluta ipsa amet
-            placeat velit temporibus!
-          </p>
+              <p>
+                Dette prosjektet er bygget på MERN stacken. Noe som vil si at vi
+                bruker Mongodb til database, Express til å bygge backend,
+                React.js til å bygge frontend og Node.js som er en plattform til
+                å lage JavaScript applikasjoner.
+              </p>
+
+              <p className='u-margin-bottom-large'>
+                For å kunne autentisere har vi brukt Passport med Json Web
+                Tokens for å verifisere om brukeren er logget inn eller ikke.
+                Dataene blir lagret i en database hos Mongodb. Alle passord blir
+                kryptert med bcrypt før de lagres.
+              </p>
+
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Forbedringer
+              </h2>
+
+              <p>
+                Skulle jeg gjort om igjen dette prosjektet burde jeg ha laget en
+                sikrere innlogging. Det bør gå over http secure og ha refresh
+                tokens i stedet for å bare bruke den samme så lenge brukeren er
+                logget inn slik som det fungerer i dag.
+              </p>
+
+              <p>
+                Prosjektet er bygget i React.js, men for enklere ruting og bedre
+                SEO ville jeg ha brukt Next.js neste gang. Det gjør det mulig
+                for å skalere lettere og bruke en blanding av Static Side
+                Generated og Server Side Rendered for raskest mulig rendring av
+                siden.
+              </p>
+            </>
+          ) : (
+            <>
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Background
+              </h2>
+
+              <p className='u-margin-bottom-large'>
+                At NTNU&apos;s locations at Mustad, they have many plants to
+                keep track of. Who watered, when were they last watered, and the
+                locations? The design department needs an administrative tool to
+                monitor the condition and health of the plants. This project
+                makes both the backend API and the frontend. The project is a
+                group work where we were three people working on the same
+                project.
+              </p>
+
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Technologies
+              </h2>
+
+              <p>
+                This project is built on the MERN stack. This is to say that we
+                use MongoDB for the database, Express to build the backend,
+                React.js to build the frontend, and Node.js which is a platform
+                to create JavaScript applications.
+              </p>
+
+              <p className='u-margin-bottom-large'>
+                To be able to authenticate, we have used Passport with Json Web
+                Tokens to verify whether the user is logged in or not. The data
+                is stored in a database at MongoDB. All passwords are encrypted
+                with bcrypt before being stored.
+              </p>
+
+              <h2 className='heading-secondary u-margin-bottom-small'>
+                Improvements
+              </h2>
+
+              <p>
+                If I had to do this project over again, I should have made a
+                more secure login system. It should have gone over HTTP secure
+                and have refresh tokens instead of just using the same one for
+                as long as the user is logged in as it works today.
+              </p>
+
+              <p>
+                The project is built in React.js, but for easier routing and
+                better SEO, I would have used Next.js next time. It makes it
+                possible to scale more easily and uses a mix of Static Side
+                Generated and Server Page Rendered for the fastest possible
+                rendering of the page.
+              </p>
+            </>
+          )}
         </div>
       </main>
     </div>
